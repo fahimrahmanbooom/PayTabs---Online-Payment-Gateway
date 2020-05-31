d7d5b6a01ca5339522022d4e94fb74a563dcda76
circuit <fahimrahmanbooom@gmail.com>
2020-05-31 20:01:08 +0600

    initial

---
+++ Paytabs Demo/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Paytabs Demo
+//
+//  Created by Fahim Rahman on 31/5/20.
+//  Copyright © 2020 Fahim Rahman. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <paytabs-iOS/paytabs_iOS.h>
+
+#endif /* Header_h */
